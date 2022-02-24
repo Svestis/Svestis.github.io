2072dc60fc8393ea3ddaa2d12d2ae6232bb3feaa
Panagiotis Koilakos <45639356+Svestis@users.noreply.github.com>
2022-02-24 19:54:05 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,115 @@
+function monthDiff(d) {
+  var months;
+  var today = new Date;
+  months = (today.getFullYear() - d.getFullYear())*12;
+  months -= d.getMonth();
+  months += today.getMonth();
+  return months<=0?0: months;
+}
+
+function years() {
+  var d = new Date('01' + 'September 2014');
+  first = d.getFullYear();
+
+  second = new Date().getFullYear();
+
+  arr = Array();
+  for (i = first; i<= second; i++) arr.push(i);
+  console.log(arr);
+}
+
+// Bar Chart Main Page
+Chart.defaults.plugins.tooltip.enabled = false;
+Chart.defaults.plugins.legend.display = false;
+
+
+var canvasElement = document.getElementById("chart-cont").getContext('2d');
+
+var config = {
+  type: "bar",
+  data: {
+    labels: ["Data Management", "Data Analysis", "Data Viz", "Python", "SQL"],
+    datasets: [{
+      data: [Math.round((monthDiff(new Date(2016, 10,31))/12)*2)/2, Math.round((monthDiff(new Date(2016, 10,31))/12)*2)/2, Math.round((monthDiff(new Date(2016, 10,31))/12)*2)/2, Math.round((monthDiff(new Date(2015, 6,30))/12)*2)/2, Math.round((monthDiff(new Date(2017, 12,31))/12)*2)/2],
+      backgroundColor: ['rgb(220,233,255)', 'rgb(184, 201, 238)', 'rgb(207,255,242)', 'rgb(255,231,232)', 'rgb(255,249,203)'],
+      borderColor: ['rgb(88,155,229)', 'rgb(80, 100, 137)', 'rgb(73,207,180)', 'rgb(255,132,144)', 'rgb(250,235,0)'],
+      borderWidth: 1,
+      datalabels: {
+        anchor: 'end',
+        align: 'top',
+        offset: 5
+      }
+    }],
+  },
+  plugins: [ChartDataLabels],
+  options: {
+    gridlines: {
+      display:false,
+    },
+    scales: {
+      yAxis: {
+        display: false,
+        ticks: {
+          display: false,
+        },
+      },
+      xAxis: {
+        ticks: {
+          fontSize: 40
+        }
+      }
+    },
+    plugins: {
+      subtitle: {
+        display: true,
+        text: 'years of experience',
+        padding: 10
+      },
+    },
+  }
+};
+
+var chart = new Chart(canvasElement, config);
+
+// Line graph Main Page
+
+var canvasElement2 = document.getElementById("chart-cont2").getContext('2d');
+
+years = years()
+
+var config2 = {
+  type: "line",
+  data: {
+    labels: years,
+    datasets: [{
+      label: "My first dataset",
+      data: [10, 20, 30, 40, 50, 60, 70, 80, 90],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1
+    }]
+  },
+   plugins: [ChartDataLabels],
+  options: {
+    gridlines: {
+      display:false,
+    },
+    scales: {
+      yAxis: {
+        display: false,
+        ticks: {
+          display: false,
+        },
+      },
+    },
+    plugins: {
+      subtitle: {
+        display: true,
+        text: 'learning curve',
+        padding: 10
+      },
+    },
+  }
+};
+
+var chart2 = new Chart(canvasElement2, config2);
